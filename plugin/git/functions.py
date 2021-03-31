6deff683ba09d31c741e8b82bcd674a23ad23761
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2021-03-31 10:21:53 +0200

    Fixed the plugin.git.describe function

--- plugin/git/functions.py
+++ plugin/git/functions.py
@@ -1,5 +1,5 @@
 from ._repo import _get_repo
 
 
-def describe(path: str) -> str:
-    return _get_repo(path).git.describe(always=True)
+def describe(path: str, tags: bool = True, always: bool = True) -> str:
+    return _get_repo(path).git.describe(tags=tags, always=always)
